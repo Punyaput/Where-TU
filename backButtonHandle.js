2d64131187852540efbc85e41af4f795f9088311
Punyaput <punyaput.kae@dome.tu.ac.th>
2024-11-11 22:39:05 +0700

    Update backButtonHandle.js

--- backButtonHandle.js
+++ backButtonHandle.js
@@ -1,1 +1,30 @@
+window.history.pushState("","","");
+
+let historyLog = [];
+let flipper = 0;
+
+window.addEventListener('popstate', goToPage);
+
+function goToPage() {
+  let pageName = historyLog[historyLog.length - 2];
+  if (pageName == undefined || pageName == 'home') {
+    closeFavoriteBox();
+    closeFeedbackBox();
+    closeInfoBox();
+    favButtonToggle = false;
+    feedbackToggle = false;
+  }
+  else if (pageName == 'favorite') {
+    favoriteButton();
+  }
+  else if (pageName == 'feedback') {
+    feedbackButton();
+  }
+  else {
+    selectInput(pageName)
+  }
+  window.history.pushState("","","");
+
+  historyLog.pop();
+}
 
