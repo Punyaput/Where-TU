29f550656a24f810c1699b721b04b97b281746b9
Punyaput <punyaput.kae@dome.tu.ac.th>
2024-11-11 21:47:52 +0700

    Update backButtonHandle.js

--- backButtonHandle.js
+++ backButtonHandle.js
@@ -1,18 +1,1 @@
-// Push a state to the history to detect back actions
-window.history.pushState({ page: 1 }, "", "");
 
-// Define the function you want to run when back button is pressed
-function onBackButtonPressed() {
-  console.log("Back button pressed! Running custom function...");
-  alert("Custom function executed on back button press!");
-  // Add any other logic you need to run here
-}
-
-// Listen for the 'popstate' event to detect history change
-window.addEventListener("popstate", function (event) {
-  // Call the custom function when back button is detected
-  onBackButtonPressed();
-
-  // Optionally prevent going back by pushing the state again
-  window.history.pushState({ page: 1 }, "", "");
-});
